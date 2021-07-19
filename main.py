d17d71c9ef218ef3823638e834a98c844b19c1b7
Gaurav-Jain-0781 <gauravjain0781@gmail.com>
2021-07-19 21:40:33 +0530

    added anvigation and other functions

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ posts_data = {
 
 @app.route('/')
 def home_page():
-    return "my first flask app ."
+    return render_template('home.html', post=posts_data)
 
 
 @app.route('/post/<int:post_id>')
@@ -23,23 +23,20 @@ def returning_post(post_id):
     return render_template('post_html.html', post=posts)
 
 
-@app.route('/post/form')
-def post_form():
+@app.route('/post/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form.get('Title')
+        content = request.form.get('content')
+        new_post_id = len(posts_data)
+        posts_data[new_post_id] = {
+            'post_id': new_post_id,
+            'title': title,
+            'content': content
+        }
+        return redirect(url_for('returning_post', post_id=new_post_id))
     return render_template('form.html', message='form for creating post .')
 
-
-@app.route('/post/create', methods=['POST'])
-def create():
-    title = request.form.get('Title')
-    content = request.form.get('content')
-    new_post_id = len(posts_data)
-    posts_data[new_post_id] = {
-        'post_id': new_post_id,
-        'title': title,
-        'content': content
-    }
-    return redirect(url_for('returning_post', post_id=new_post_id))
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
