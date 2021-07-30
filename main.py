b8b675ac8d2f9ec805e8d682ce1aa00651855514
Gaurav-Jain-0781 <gauravjain0781@gmail.com>
2021-07-30 23:26:51 +0530

    function corrected

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import sqlite3
 app = Flask(__name__)
 
 posts_data = {
-        0: {
-            'post_id': 1,
-            'title': 'My First Blog',
-            'content': 'Hello , world !!',
-            'author': 'gaurav jain'
-        },
-        }
+    0: {
+        'post_id': 1,
+        'title': 'My First Blog',
+        'content': 'Hello , world !!',
+        'author': 'gaurav jain'
+    },
+}
 
 
 def create_blog_table():
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
 
-    cursor.execute('CREATE TABLE IF NOT EXISTS blog (post_id integer, title text, content text)')
     cursor.execute('CREATE TABLE IF NOT EXISTS blog (post_id integer, title text, content text, author text)')
 
     connection.commit()
@@ -26,7 +25,7 @@ def create_blog_table():
 
 def add_blog(data):
     post_id = data['post_id']
-    title = data['title'].capitalize()
+    title = data['title']
     content = data['content']
     author = data['author']
 
@@ -42,14 +41,6 @@ def add_blog(data):
 def retrive_data():
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
-
-    cursor.execute('SELECT post_id, title, content, author FROM blog')
-
-    blog = [{'post_id': row[0],
-             'title': row[1],
-             'content': row[2]},
-            'author': row[3]
-            for row in cursor.fetchall()}
 
     cursor.execute('SELECT post_id, title, content, author FROM blog')
 
@@ -82,14 +73,14 @@ def home_page():
 def returning_post(post_id):
     posts = posts_data.get(post_id)
     if not posts:   # if not post => true
-        return render_template('error.html', message=f'post with id {post_id} was not found . ')
+        return render_template('error.html', message=f'Post with id {post_id} was not found . ')
     return render_template('post_html.html', post=posts)
 
 
 @app.route('/post/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        title = request.form.get('Title').capitalize()
+        title = request.form.get('Title')
         content = request.form.get('content')
         author = request.form.get('author')
 
@@ -105,7 +96,7 @@ def create():
         return redirect(url_for('returning_post', post_id=new_post_id))
     return render_template('form.html', message='Form for Creating Post')
 
-retrive_data()
+
 @app.route('/update')
 def update():
     return render_template('update.html', message='Form to Update an Existing Post')
@@ -174,8 +165,9 @@ def home_page_update(title, content, author):
     connection.close()
 
 
+create_blog_table()
+
 if __name__ == '__main__':
     create_blog_table()
-    app.run(debug=True)
     retrive_data()
     app.run(debug=True)
